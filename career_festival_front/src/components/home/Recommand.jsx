d0031b906d1950b307a89b7486b3d2d2fb65b769
dadesign0709 <dadesign0709@gmail.com>
2024-01-17 20:19:07 +0900

    Refactor: footer styled-components 일부 수정 Docs: Recommand components 수정

--- career_festival_front/src/components/home/Recommand.jsx
+++ career_festival_front/src/components/home/Recommand.jsx
@@ -1,26 +1,54 @@
 // src/components/home/Recommand.jsx
 import React from 'react';
+import styled from 'styled-components';
+
+const RecommandCardcontainer = styled.div`
+
+`
 
 const Recommand = () => {
-  // 예시 데이터 (행사 목록)
-  const recommandedEvents = [
-    { id: 1, title: '좋은 기업 채용박람회', date: '2024-02-15' },
-    { id: 2, title: 'IT 기술 세미나', date: '2024-03-10' },
-    { id: 3, title: '디자인 워크샵', date: '2024-04-05' },
+  // 예시 데이터 (위치 기반 행사 목록)
+  const recommandedEventsPlace = [
+    { id: 1, title: '좋은 기업 채용박람회', startdate: '2024-02-15', enddate:'2024-02-16' },
+    { id: 2, title: 'IT 기술 세미나', startdate: '2024-03-10', enddate:'2024-03-11' },
+    { id: 3, title: '디자인 워크샵1', startdate: '2024-04-05', enddate:'2024-04-06' },
+    { id: 4, title: '디자인 워크샵2', startdate: '2024-04-06', enddate:'2024-04-07' },
+    { id: 5, title: '디자인 워크샵3', startdate: '2024-04-07', enddate:'2024-04-08' },
+    { id: 6, title: '디자인 워크샵4', startdate: '2024-04-08', enddate:'2024-04-09' },
+  ];
+
+  // 예시 데이터 (개인 기반 행사 목록)
+  const recommandedEventsPersonal = [
+    { id: 1, title: '좋은 기업 채용박람회2', startdate: '2024-02-15', enddate:'2024-02-16' },
+    { id: 2, title: 'IT 기술 세미나2', startdate: '2024-03-10', enddate:'2024-03-11' },
+    { id: 3, title: '디자인 워크샵12', startdate: '2024-04-05', enddate:'2024-04-06' },
   ];
 
   return (
-    <div>
+   /* <div>
       <h3>추천 행사 사진</h3>
       <ul>
-        {recommandedEvents.map(event => (
+        {recommandedEventsPlace.map(event => (
           <li key={event.id}>
             <strong>{event.title}</strong> - {event.date}
           </li>
         ))}
       </ul>
-    </div>
+    </div>*/
+    <RecommandCardcontainer>
+      <h3>추천 행사 사진</h3>
+      <ul>
+        {recommandedEventsPlace.map(event => (
+          <li key={event.id}>
+            <strong>{event.title}</strong> - {event.date}
+          </li>
+        ))}
+      </ul>
+    </RecommandCardcontainer>
   );
 };
 
 export default Recommand;
+
+
+
