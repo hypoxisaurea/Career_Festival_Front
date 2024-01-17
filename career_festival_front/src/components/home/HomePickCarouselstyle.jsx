d3693e3b6096d6adbf4541b50cc9f4a6c02d9f5e
dadesign0709 <dadesign0709@gmail.com>
2024-01-18 01:29:11 +0900

    feat: HomePage 기본 틀잡기 - dummy 데이터 추가 - Recommand component 틀잡기

--- career_festival_front/src/components/home/HomePickCarouselstyle.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/home/HomePageStyle.jsx
-import styled from 'styled-components';
-
-const HomePickCarouselContainer = styled.div`
- font-weight: bold;
-`;
-
-export {HomePickCarouselContainer};
--- career_festival_front/src/components/home/Recommand.jsx
+++ career_festival_front/src/components/home/Recommand.jsx
@@ -1,49 +1,46 @@
 // src/components/home/Recommand.jsx
 import React from 'react';
 import styled from 'styled-components';
+import dummy from "../../db/RecommandedEvents.json";
 
 const RecommandCardcontainer = styled.div`
+  width: 392px;
+  height: 400px;
+  margin: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid red;
+  border-radius: 32px;
 
+  img{
+    width: 392px;
+    height: 246px;
+    border-radius: 32px;
+    margin: 10px 0px;
+  }
+
+  h4{
+    margin: 0px;
+  }
+`
+const RecommmadInfoWrapper = styled.div`
+  width: 392px;
+  height: 132px;
+ 
+  border: 1px solid red;
 `
 
-const Recommand = () => {
-  // 예시 데이터 (위치 기반 행사 목록)
-  const recommandedEventsPlace = [
-    { id: 1, title: '좋은 기업 채용박람회', startdate: '2024-02-15', enddate:'2024-02-16' },
-    { id: 2, title: 'IT 기술 세미나', startdate: '2024-03-10', enddate:'2024-03-11' },
-    { id: 3, title: '디자인 워크샵1', startdate: '2024-04-05', enddate:'2024-04-06' },
-    { id: 4, title: '디자인 워크샵2', startdate: '2024-04-06', enddate:'2024-04-07' },
-    { id: 5, title: '디자인 워크샵3', startdate: '2024-04-07', enddate:'2024-04-08' },
-    { id: 6, title: '디자인 워크샵4', startdate: '2024-04-08', enddate:'2024-04-09' },
-  ];
-
-  // 예시 데이터 (개인 기반 행사 목록)
-  const recommandedEventsPersonal = [
-    { id: 1, title: '좋은 기업 채용박람회2', startdate: '2024-02-15', enddate:'2024-02-16' },
-    { id: 2, title: 'IT 기술 세미나2', startdate: '2024-03-10', enddate:'2024-03-11' },
-    { id: 3, title: '디자인 워크샵12', startdate: '2024-04-05', enddate:'2024-04-06' },
-  ];
-
+const Recommand = ({mainImg, eventName, recruitmentStart, recruitmentEnd, price}) => {
   return (
-   /* <div>
-      <h3>추천 행사 사진</h3>
-      <ul>
-        {recommandedEventsPlace.map(event => (
-          <li key={event.id}>
-            <strong>{event.title}</strong> - {event.date}
-          </li>
-        ))}
-      </ul>
-    </div>*/
     <RecommandCardcontainer>
-      <h3>추천 행사 사진</h3>
-      <ul>
-        {recommandedEventsPlace.map(event => (
-          <li key={event.id}>
-            <strong>{event.title}</strong> - {event.date}
-          </li>
-        ))}
-      </ul>
+      <img src={mainImg} alt='행사 이미지'/>
+      <RecommmadInfoWrapper>
+        <h4>{eventName}</h4>
+        <div>{recruitmentStart}</div>
+        <div>{recruitmentEnd}</div>
+        <div>{price}</div>
+      </RecommmadInfoWrapper>
     </RecommandCardcontainer>
   );
 };
