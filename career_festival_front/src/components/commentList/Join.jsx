cb133d0172d218656d4d177452b8d3256ed7612d
hypoxisaurea <flickerlight14@naver.com>
2024-01-21 20:27:54 +0900

    Edit DetailPage

---
+++ career_festival_front/src/components/commentList/Join.jsx
@@ -0,0 +1,170 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const JoinContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 157px 0 0 0;
+  display: block;
+`;
+
+const AskContainer = styled.div`
+  width: 1224px;
+  height: 100%;
+  border-radius: 10px;
+  border: 1px #838383 solid;
+  margin: 50px 0 42px 0;
+`;
+
+const UserContainer = styled.div`
+  width: 100%;
+  height: 23px;
+  margin: 29px 0 0 25px;
+  display: flex;
+`;
+
+const AskContentContainer = styled.textarea`
+  width: 1180px;
+  min-height: 99px;
+  border-radius: 10px;
+  border: 3px #838383 solid;
+  margin: 9px 19px 0 24px;
+
+  &::placeholder {
+    color: #838383;
+    font-size: 14px;
+    font-family: "Noto Sans KR";
+    font-weight: 400;
+  }
+`;
+
+const CountContainer = styled.div`
+  width: 72px;
+  height: 100%;
+  display: flex;
+`;
+
+const ReplyContainer = styled.div`
+  width: 203px;
+  height: 49px;
+  background: #020d6a;
+  border-bottom-right-radius: 10px;
+  margin-left: 1021px;
+
+  color: white;
+  font-size: 16px;
+  font-family: "Noto Sans KR";
+  font-weight: 700;
+  word-wrap: break-word;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HorizontalDivider = styled.div`
+  width: 100%;
+  height: 1px;
+  background: black;
+  margin: 23px 0 0 0;
+`;
+
+const Subtitle = styled.div`
+  color: black;
+  font-size: 20px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+`;
+
+const ContentText = styled.div`
+  color: black;
+  font-size: 15px;
+  font-family: "Noto Sans KR";
+  font-weight: 400;
+  word-wrap: break-word;
+  margin: 16px 0 0 0;
+`;
+
+const UserName = styled.span`
+  color: black;
+  font-size: 16px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+  text-decoration: underline;
+`;
+
+const UserRole = styled.div`
+  border-radius: 9.5px;
+  border: 1px #582fff solid;
+
+  color: #582fff;
+  font-size: 14px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+
+  display: inline-block;
+  padding: 0 8px 0 8px;
+  margin-left: 4px;
+`;
+
+const QnAText = styled.span`
+  color: black;
+  font-size: 16px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+  margin-left: 3px;
+`;
+
+const CountText = styled.span`
+  color: black;
+  font-size: 14px;
+  font-family: "Noto Sans KR";
+  font-weight: 700;
+  word-wrap: break-word;
+`;
+
+const CountColorText = styled.span`
+  color: #582fff;
+  font-size: 14px;
+  font-family: "Noto Sans KR";
+  font-weight: 700;
+  word-wrap: break-word;
+`;
+
+const Join = () => {
+  return (
+    <JoinContainer>
+      <Subtitle>같이 가요!</Subtitle>
+      <ContentText>
+        같이 갈 팀원을 모집하고 싶다면 팀장이 되어 일정을 잡아보세요!
+        <br />
+        팀원들은 팀장의 글에 답글을 남겨 참여할 수 있습니다.
+      </ContentText>
+      <AskContainer>
+        <UserContainer>
+          <UserName>김커리</UserName>
+          <QnAText>님</QnAText>
+          <UserRole>팀장</UserRole>
+        </UserContainer>
+
+        <AskContentContainer
+          placeholder="팀원들과 같이 갈 날짜와 시간, 장소와 인원수를 명시해주세요!  팀원들이 모일 수단을 첨부하시면 도움이 됩니다.
+          예시) 1월 14일 14시, 삼각지역 9번 출구 앞, 3명, https://open.kakao.com/o/uIPzxlZf"
+        />
+        <HorizontalDivider />
+
+        <CountContainer>
+          <CountColorText>0</CountColorText>
+          <CountText>/5000자</CountText>
+        </CountContainer>
+        <ReplyContainer>댓글 등록</ReplyContainer>
+      </AskContainer>
+    </JoinContainer>
+  );
+};
+
+export default Join;
---
+++ career_festival_front/src/components/qna/QnAList.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const TitleContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 42px 0 18px 0;
+`;
+
+const QnAListContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: block;
+`;
+
+const QnAContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: block;
+  margin-bottom: 25px;
+`;
+
+const InfoContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+`;
+
+const Subtitle = styled.span`
+  color: black;
+  font-size: 16px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+  margin-left: 3px;
+`;
+
+const SubtitleColor = styled.span`
+  color: #582fff;
+  font-size: 16px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+`;
+
+const UserNameText = styled.span`
+  color: black;
+  font-size: 16px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+  margin-right: 5px;
+`;
+
+const DateText = styled.span`
+  color: black;
+  font-size: 14px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+  margin-right: 5px;
+`;
+
+const AnswerText = styled.span`
+  color: #582fff;
+  font-size: 14px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+`;
+
+const ContentText = styled.span`
+  color: #838383;
+  font-size: 14px;
+  font-family: "Noto Sans KR";
+  font-weight: 500;
+  word-wrap: break-word;
+`;
+
+const QnAList = () => {
+  return (
+    <div>
+      <TitleContainer>
+        <SubtitleColor>2</SubtitleColor>
+        <Subtitle>개의 문의</Subtitle>
+      </TitleContainer>
+
+      <QnAListContainer>
+        <QnAContainer>
+          <InfoContainer>
+            <UserNameText>최**</UserNameText>
+            <DateText>2023/12/25</DateText>
+            <AnswerText>미답변</AnswerText>
+          </InfoContainer>
+          <ContentText>사용자가 비공개처리 했습니다.</ContentText>
+        </QnAContainer>
+
+        <QnAContainer>
+          <InfoContainer>
+            <UserNameText>김**</UserNameText>
+            <DateText>2023/12/29</DateText>
+            <AnswerText>답변완료</AnswerText>
+          </InfoContainer>
+          <ContentText>고등학생도 참가 가능한가요?</ContentText>
+        </QnAContainer>
+      </QnAListContainer>
+    </div>
+  );
+};
+
+export default QnAList;
