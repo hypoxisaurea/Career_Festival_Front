31cea2721e2a0ee019057be0e06e1502c219c764
Taeksin Kim <yoyo2521@naver.com>
2024-01-26 06:10:36 +0900

    /signup -> /join add FestivalHistory

--- career_festival_front/src/components/diary/Other.jsx
+++ career_festival_front/src/components/diary/Other.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Dropdown from "./Dropdown";
 import EventInfo from "./EventInfo";
+import FestivalHistory from "./FestivalHistory";
 
 const OtherContainer = styled.div`
   margin: 3.5vw 23vw 3vw 23vw;
@@ -10,9 +11,11 @@ const OtherContainer = styled.div`
 function Other() {
   return (
     <OtherContainer>
+      택신<br/>
       Other<br/>
       <Dropdown />
       <EventInfo />
+      <FestivalHistory/>
     </OtherContainer>
   );
 }
