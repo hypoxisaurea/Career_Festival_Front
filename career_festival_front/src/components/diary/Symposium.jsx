8c3452d01724309508afcea81e4bb411db774f4e
Taeksin Kim <yoyo2521@naver.com>
2024-01-27 05:06:37 +0900

    add FestivalHistory  & PeopleNetwork

--- career_festival_front/src/components/diary/Symposium.jsx
+++ career_festival_front/src/components/diary/Symposium.jsx
@@ -3,9 +3,21 @@ import styled from "styled-components";
 import Dropdown from "./Dropdown";
 import EventInfo from "./EventInfo";
 import FestivalHistory from "./FestivalHistory";
+import PeopleNetwork from "./PeopleNetwork";
 
 const SymposiumContainer = styled.div`
   margin: 3.5vw 23vw 3vw 23vw;
+`;
+
+const Button = styled.button`
+  margin-top: 10px;
+  padding: 10px;
+  background-color: ${(props) => (props.disabled ? "#ccc" : "#4caf50")};
+  color: white;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
 `;
 
 function Symposium() {
@@ -16,6 +28,7 @@ function Symposium() {
       <Dropdown />
       <EventInfo />
       <FestivalHistory/>
+      <PeopleNetwork/>
     </SymposiumContainer>
   );
 }
