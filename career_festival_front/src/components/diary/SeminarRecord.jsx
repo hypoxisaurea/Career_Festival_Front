1a2ca406a4f808579660213b120ee12f2df15c3a
shawn_its <psh2968@naver.com>
2024-02-19 04:59:57 +0900

    일단 글쓰기 업로드 수정

--- career_festival_front/src/components/diary/SeminarRecord.jsx
+++ career_festival_front/src/components/diary/SeminarRecord.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
-import Modal from "../diary/Modal";
 
 import Editor from "../EditorComponent";
 
@@ -21,28 +21,10 @@ const ContentContainer = styled.div`
 `;
 
 const TextContainer = styled.div`
-  width: 100%;
-  display: flex;
-  input{
-    font-size:1vw;
+  margin-top: 1vw;
+  input {
+    font-size: 1vw;
   }
-`;
-
-// const BottomContainer = styled.div`
-//   width: 100%;
-//   display: flex;
-//   border: 0.1vw #838383 solid;
-//   border-bottom-left-radius: 0.6vw;
-//   border-bottom-right-radius: 0.6vw;
-// `;
-
-const CountContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: flex-end;
-  margin: 1vw 1vw 1.5vw 0;
-  font-size:1vw;
 `;
 
 const TitleText = styled.div`
@@ -53,61 +35,10 @@ const TitleText = styled.div`
   word-wrap: break-word;
 `;
 
-// const TextInput = styled.textarea`
-//   width: 100%;
-//   height: auto;
-//   min-height: 50vw;
-//   color: #838383;
-//   font-size: 1vw;
-//   font-family: "Noto Sans KR";
-//   font-weight: 400;
-//   word-wrap: break-word;
-//   margin-top: 1.2vw;
-//   border-top-left-radius: 0.6vw;
-//   border-top-right-radius: 0.6vw;
-//   border: 0.1vw #838383 solid;
-//   padding: 0.5vw 0 0 1vw;
-//   box-sizing: border-box;
-
-//   &::placeholder {
-//     color: #838383;
-//     font-size: 1vw;
-//     font-family: "Noto Sans KR";
-//     font-weight: 400;
-//   }
-// `;
-
-const CountText = styled.span`
-  color: black;
-  font-size: 14px;
-  font-family: "Noto Sans KR";
-  font-weight: 700;
-  word-wrap: break-word;
-`;
-
-const CountColorText = styled.span`
-  color: #582fff;
-  font-size: 14px;
-  font-family: "Noto Sans KR";
-  font-weight: 700;
-  word-wrap: break-word;
-`;
-
 function SeminarRecord({ onComplete }) {
-  const [inputCount, setInputCount] = useState(0);
-  const [isWritten, setIsWritten] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [image, setImage] = useState(null);
-  const [imageWidth, setImageWidth] = useState(0);
-  const [imageHeight, setImageHeight] = useState(0);
+  const [isWritten] = useState(false);
+  const [desc, setDesc] = useState("");
 
-    const [desc, setDesc] = useState("");
-    function onEditorChange(value) {
-      setDesc(value);
-    }
-    
-
-  // 작성이 완료되었을 때 onComplete 호출
   useEffect(() => {
     if (isWritten) {
       console.log("작성이 완료되었습니다.");
@@ -118,38 +49,17 @@ function SeminarRecord({ onComplete }) {
     }
   }, [isWritten, onComplete]);
 
-  console.log("현재 작성 여부:", isWritten);
-
-  //모달
-  const onInputHandler = (e) => {
-    const count = e.target.value.length;
-
-    setInputCount(count);
-    setIsWritten(count > 100); // 여기에서 작성 여부 판별 조건을 설정
-
-    if (count > 5000) {
-      setShowModal(true);
-    } else {
-      setShowModal(false);
-    }
-  };
-
-  const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      setImage(reader.result);
-
-      const img = new Image();
-      img.src = reader.result;
-      img.onload = () => {
-        setImageWidth(img.width);
-        setImageHeight(img.height);
-      };
-    };
-
-    reader.readAsDataURL(file);
+  const onSaveData = () => {
+    axios
+      .post("/api/saveData", { desc })
+      .then((response) => {
+        console.log("데이터 저장 완료", response.data);
+        onComplete(true);
+      })
+      .catch((error) => {
+        console.error("데이터 저장 중 오류 발생", error);
+        onComplete(false);
+      });
   };
 
   return (
@@ -159,35 +69,19 @@ function SeminarRecord({ onComplete }) {
         <ContentContainer>
           <TextContainer>
             <div>
-              <Editor value={desc} onChange={onEditorChange} />
+              <Editor value={desc} onChange={(value) => setDesc(value)} />
             </div>
-            {/* <TextInput
-              placeholder="다녀온 강연/세미나 내용을 정리해주세요."
-              onChange={onInputHandler}
-              maxLength="5000"
-            /> */}
           </TextContainer>
-          <input type="file" accept="image/*" onChange={handleImageUpload} />
-          {image && (
-            <img
-              src={image}
-              alt="Uploaded Image"
-              style={{
-                width: "100%",
-                height: "auto",
-                maxWidth: "20%",
-                maxHeight: "20%",
-              }}
-            />
-          )}
-          {showModal && <Modal onClose={() => setShowModal(false)} />}
-          <CountContainer>
-            <CountColorText>{inputCount}</CountColorText>
-            <CountText>/5000자</CountText>
-          </CountContainer>
+          <div className="pd12">
+            <button
+              className="lf-button primary"
+              onClick={onSaveData}
+            >
+              데이터 저장
+            </button>
+          </div>
         </ContentContainer>
       </InputContainer>
-      {isWritten && <div>작성이 완료되었습니다.</div>}
     </RecordContainer>
   );
 }
